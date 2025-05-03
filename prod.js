cdf8f5e03f54e891a20aec7e6da8c98af3c165a9
dina <20Ahmed.Ammar10@gmail.com>
2025-05-03 17:57:09 +0300

    casual-Teshirt

---
+++ prod.js
@@ -0,0 +1,20 @@
+let productImg = document.getElementById("prodimg");
+let btn = document.getElementsByClassName("btn");
+
+
+btn[0].onclick = function() {
+    productImg.src = "https://m.media-amazon.com/images/I/61N-ppw7uUL._AC_SX522_.jpg"; 
+};
+
+btn[1].onclick = function() {
+    productImg.src = "https://m.media-amazon.com/images/I/61d0FGjxKRL._AC_SX522_.jpg"; 
+    
+}
+
+btn[2].onclick = function() {
+    productImg.src = "https://m.media-amazon.com/images/I/61sAImooPoL._AC_SX522_.jpg"; 
+    
+};
+
+
+
